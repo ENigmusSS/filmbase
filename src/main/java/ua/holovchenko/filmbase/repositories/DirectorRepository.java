13a297711e5ee2e82471906a924cbd77c67f7595
ENigmusSS <128493758+ENigmusSS@users.noreply.github.com>
2024-05-02 23:58:58 +0300

    raw code

--- src/main/java/ua/holovchenko/filmbase/repositories/DirectorRepository.java
+++ src/main/java/ua/holovchenko/filmbase/repositories/DirectorRepository.java
@@ -2,9 +2,6 @@ package ua.holovchenko.filmbase.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import ua.holovchenko.filmbase.entities.Director;
 
 public interface DirectorRepository extends JpaRepository<Director, Long>, JpaSpecificationExecutor<Director> {
